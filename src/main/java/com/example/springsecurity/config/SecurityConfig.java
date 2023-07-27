667586cbda9acdc37b634d0f9342c47c16196d35
UJJWAL'S_WORLD <kumarujjwal471@gmail.com>
2023-07-27 11:42:25 +0530

    spring security

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity){
+
+        return httpSecurity.csrf().disable()
+    }
+}
